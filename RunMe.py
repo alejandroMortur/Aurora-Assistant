0ba56d25c7729886cf2ef78b909f8e88e0bec0cd
AlexDEV <moronturielalejandro@gmail.com>
2024-04-27 20:24:05 +0200

    base change  to new upload

--- RunMe.py
+++ RunMe.py
@@ -6,8 +6,8 @@ dependencies_path = os.path.abspath('./src/resources/dependences')
 sys.path.insert(0, dependencies_path)
 
 # Then import your necessary modules and run your main code
-import speech_recognition as rs
+import speech_recognition as sr
 import pyttsx3, pywhatkit
 from src.virtual_assistant import Main
 
-Main.main()
+Main.main(sr, pyttsx3, pywhatkit)
--- src/virtual_assistant/Main.py
+++ src/virtual_assistant/Main.py
@@ -1,5 +1,42 @@
-def main():
-    print("¡Hola, mundo!")
+def main(sr, pyttsx3, pywhatkit):
+    name = "Aurora"
+    listener = sr.Recognizer()
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    
+    try:
+        rec = listen(listener,sr)
+        if 'reproduce' in rec:
+            music = rec.replace('reproduce','')
+            print("Reproduciendo: " + music)
+            talk(engine, "Reproduciendo: " + music)
+            pywhatkit.playonyt(music)
+    except Exception as e:
+        print("Error:", e)
+
+def talk(engine, text):
+    engine.say(text)
+    engine.runAndWait()
+    
+def listen(listener,sr):
+    rec = ""  # Inicializa rec con un valor predeterminado
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando....")
+            pc = listener.listen(source)
+            rec = listener.recognize_google(pc)
+            rec = rec.lower()
+            if name in rec:
+                rec = rec.replace(name,'')
+    except sr.UnknownValueError:
+        print("No se pudo entender el audio")
+    except sr.RequestError as e:
+        print("Error en la solicitud de reconocimiento de voz; {0}".format(e))
+    except Exception as e:
+        print("Error:", e)
+    return rec
+
 
 if __name__ == "__main__":
     main()

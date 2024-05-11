c0b427ecafa3060b1bb721dbd1b3321666a43681
AlexDEV <moronturielalejandro@gmail.com>
2024-05-11 20:11:38 +0200

    main fix

--- src/virtual_assistant/Main.py
+++ src/virtual_assistant/Main.py
@@ -4,59 +4,67 @@ from GenerateModule import generateAudio
 from MicHandler import getVoice
 from LLMModule import getLLMText
 from TextHandler import read_file, read_word
+defautLanguange = "es-ES"
 
-# Default language setting
-defaultLanguage = "es-ES"
-
-# File paths for different resources
 File = [
-    "./textResources/IntroText01.txt",        # Introduction text
-    "./textResources/ES/EsKeyWordsText02.txt",# Spanish keywords
-    "./textResources/ES/EsDefaultSentences01.txt", # Default Spanish sentences
-    "./textResources/EN/EnKeyWordsText02.txt",# English keywords
-    "./textResources/EN/EnDefaultSentences01.txt"  # Default English sentences
+    
+    "./textResources/IntroText01.txt",
+    "./textResources/ES/EsKeyWordsText02.txt",
+    "./textResources/ES/EsDefaultSentences01.txt",
+    "./textResources/EN/EnKeyWordsText02.txt",
+    "./textResources/EN/EnDefaultSentences01.txt"
+    
 ]
 
-# Read intro text from file
 content = read_file(File[0])
 print(content)
-generateAudio(content, defaultLanguage)
+generateAudio(content,defautLanguange)
 
-# Get user's preferred language through voice input
-language = getVoice(defaultLanguage)
-
-# Set language based on user input
-if language == "castellano":
-    generateAudio("Entendido, te hablaré en castellano a partir de ahora", defaultLanguage)
-    defaultLanguage = "es-ES"
-    keyWords = read_word(File[1])  # Load Spanish keywords
-    defaultSentences = read_word(File[2])  # Load default Spanish sentences
-    print(keyWords, defaultSentences)
-else:
-    generateAudio("Got it, from now on I will speak to you in English", defaultLanguage)
-    defaultLanguage = "en-US"
-    keyWords = read_word(File[3])  # Load English keywords
-    defaultSentences = read_word(File[4])  # Load default English sentences
-    print(keyWords, defaultSentences)
-
-# Main loop for voice interaction
-while True:
-    # Get user's response through voice input
-    response = getVoice(defaultLanguage)
-    response = response.lower()  # Convert response to lowercase for easier comparison
+languague = getVoice(defautLanguange)
+if languague == "castellano":
     
-    # Check if the wake word "aurora" is in the response
-    if "aurora" in response:
-        # Remove the wake word "aurora" from the response
-        response = response.replace("aurora", "")
-        
-        # Check for various commands based on keywords
-        if keyWords[0] in response and keyWords[1] in response or keyWords[2] in response:
-            generateAudio("Sure, tell me what you want me to search for you", defaultLanguage)
-        elif keyWords[3] in response:
-            generateAudio("Sure, which program do you want to open?", defaultLanguage)
-        # Additional commands can be added similarly
-        
-    else:
-        generateAudio("Sorry, I didn't understand you", defaultLanguage)
+    generateAudio("Entendido te hablare en castellano a partir de ahora",defautLanguange)
+    defautLanguange = "es-ES"
+    keyWords =  read_word(File[1])
+    defaultSentences =  read_word(File[2])
+    print(keyWords,defaultSentences)
+    
+else:
+    generateAudio("Got it from now on i will speak to you in english",defautLanguange)
+    defautLanguange = "en-US"
+    keyWords =  read_word(File[3])
+    defaultSentences =  read_word(File[4])
+    print(keyWords,defaultSentences)
 
+while True:
+    respond = getVoice(defautLanguange)
+    respond = respond.lower()
+    if "aurora" in respond:  
+        respond = respond.replace("aurora","")
+        if keyWords[0] in respond and keyWords[1] in respond or keyWords[2] in respond:
+            generateAudio("Claro dime que quieres que busque por ti",defautLanguange)
+        elif keyWords[3] in respond:
+            generateAudio("Claro que programa quieres abrir?",defautLanguange)
+        elif keyWords[4] in respond:
+            generateAudio("Claro ahora te digo, dejame pensar",defautLanguange)
+            textGenerated = getLLMText(respond,100)
+            generateAudio(textGenerated,defautLanguange)
+            generateAudio("¿Te ha sido util esta informacion? o quieres mas?",defautLanguange)
+            moreText = ""
+            while keyWords[5] not in moreText: 
+                moreText = getVoice(defautLanguange)
+                moreText = respond.lower()
+                if keyWords[6] in moreText or keyWords[7] in moreText or keyWords[8] in moreText:
+                    textGenerated = getLLMText(respond,200)
+                    generateAudio(textGenerated,defautLanguange)
+                    
+        elif keyWords[9] in respond:
+            generateAudio("Claro que programa quieres que cierre?",defautLanguange)
+        elif keyWords[10] in respond and "alarma" in respond:
+            generateAudio("Claro puedo hacerte una alarma, cuando la quieres?",defautLanguange)
+        elif keyWords[11] in respond:
+            generateAudio("Buenas!!!",defautLanguange)
+        else:
+            generateAudio("Lo siento no te he entendido",defautLanguange)
+    else:
+        generateAudio("Lo siento no te he entendido",defautLanguange)
